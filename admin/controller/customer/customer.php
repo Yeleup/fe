8e5695109631726e331b55286c5cbd198c27feb9
Magzhan Yeleup <magzhan9292@gmail.com>
2023-02-08 13:47:41 +0600

    Исправление ошибки при добавления пользователя в админке

--- admin/controller/customer/customer.php
+++ admin/controller/customer/customer.php
@@ -1048,49 +1048,49 @@ class ControllerCustomerCustomer extends Controller {
 			}
 		}
 
-		if (isset($this->request->post['address'])) {
-			foreach ($this->request->post['address'] as $key => $value) {
-				if ((utf8_strlen($value['firstname']) < 1) || (utf8_strlen($value['firstname']) > 32)) {
-					$this->error['address'][$key]['firstname'] = $this->language->get('error_firstname');
-				}
-
-				if ((utf8_strlen($value['lastname']) < 1) || (utf8_strlen($value['lastname']) > 32)) {
-					$this->error['address'][$key]['lastname'] = $this->language->get('error_lastname');
-				}
-
-				if ((utf8_strlen($value['address_1']) < 3) || (utf8_strlen($value['address_1']) > 128)) {
-					$this->error['address'][$key]['address_1'] = $this->language->get('error_address_1');
-				}
-
-				if ((utf8_strlen($value['city']) < 2) || (utf8_strlen($value['city']) > 128)) {
-					$this->error['address'][$key]['city'] = $this->language->get('error_city');
-				}
-
-				$this->load->model('localisation/country');
-
-				$country_info = $this->model_localisation_country->getCountry($value['country_id']);
-
-				if ($country_info && $country_info['postcode_required'] && (utf8_strlen($value['postcode']) < 2 || utf8_strlen($value['postcode']) > 10)) {
-					$this->error['address'][$key]['postcode'] = $this->language->get('error_postcode');
-				}
-
-				if ($value['country_id'] == '') {
-					$this->error['address'][$key]['country'] = $this->language->get('error_country');
-				}
-
-				if (!isset($value['zone_id']) || $value['zone_id'] == '') {
-					$this->error['address'][$key]['zone'] = $this->language->get('error_zone');
-				}
-
-				foreach ($custom_fields as $custom_field) {
-					if (($custom_field['location'] == 'address') && $custom_field['required'] && empty($value['custom_field'][$custom_field['custom_field_id']])) {
-						$this->error['address'][$key]['custom_field'][$custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
-					} elseif (($custom_field['location'] == 'address') && ($custom_field['type'] == 'text') && !empty($custom_field['validation']) && !filter_var($value['custom_field'][$custom_field['custom_field_id']], FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => $custom_field['validation'])))) {
-						$this->error['address'][$key]['custom_field'][$custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
-                    }
-				}
-			}
-		}
+//		if (isset($this->request->post['address'])) {
+//			foreach ($this->request->post['address'] as $key => $value) {
+//				if ((utf8_strlen($value['firstname']) < 1) || (utf8_strlen($value['firstname']) > 32)) {
+//					$this->error['address'][$key]['firstname'] = $this->language->get('error_firstname');
+//				}
+//
+//				if ((utf8_strlen($value['lastname']) < 1) || (utf8_strlen($value['lastname']) > 32)) {
+//					$this->error['address'][$key]['lastname'] = $this->language->get('error_lastname');
+//				}
+//
+//				if ((utf8_strlen($value['address_1']) < 3) || (utf8_strlen($value['address_1']) > 128)) {
+//					$this->error['address'][$key]['address_1'] = $this->language->get('error_address_1');
+//				}
+//
+//				if ((utf8_strlen($value['city']) < 2) || (utf8_strlen($value['city']) > 128)) {
+//					$this->error['address'][$key]['city'] = $this->language->get('error_city');
+//				}
+//
+//				$this->load->model('localisation/country');
+//
+//				$country_info = $this->model_localisation_country->getCountry($value['country_id']);
+//
+//				if ($country_info && $country_info['postcode_required'] && (utf8_strlen($value['postcode']) < 2 || utf8_strlen($value['postcode']) > 10)) {
+//					$this->error['address'][$key]['postcode'] = $this->language->get('error_postcode');
+//				}
+//
+//				if ($value['country_id'] == '') {
+//					$this->error['address'][$key]['country'] = $this->language->get('error_country');
+//				}
+//
+//				if (!isset($value['zone_id']) || $value['zone_id'] == '') {
+//					$this->error['address'][$key]['zone'] = $this->language->get('error_zone');
+//				}
+//
+//				foreach ($custom_fields as $custom_field) {
+//					if (($custom_field['location'] == 'address') && $custom_field['required'] && empty($value['custom_field'][$custom_field['custom_field_id']])) {
+//						$this->error['address'][$key]['custom_field'][$custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
+//					} elseif (($custom_field['location'] == 'address') && ($custom_field['type'] == 'text') && !empty($custom_field['validation']) && !filter_var($value['custom_field'][$custom_field['custom_field_id']], FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => $custom_field['validation'])))) {
+//						$this->error['address'][$key]['custom_field'][$custom_field['custom_field_id']] = sprintf($this->language->get('error_custom_field'), $custom_field['name']);
+//                    }
+//				}
+//			}
+//		}
 
 		if ($this->request->post['affiliate']) {
 			if ($this->request->post['payment'] == 'cheque') {
--- admin/model/fe/customer/fe_customer.php
+++ admin/model/fe/customer/fe_customer.php
@@ -24,31 +24,9 @@ class ModelFeCustomerFeCustomer extends Model {
 
     public function updateById($customer_id, $data) {
         $customer_id = (int)$customer_id;
-        $reg_type = (int)$data['reg_type'];
-        $customer_type_id = (int)$data['customer_type_id'];
-        $status = (int)$data['status'];
-        $iin = $data['iin'];
-        $company_name = $data['company_name'];
-        $company_name_full = $data['company_name_full'];
-        $official_address = $data['official_address'];
-        $address = $data['address'];
-        $client_category_id = $data['client_category_id'];
-        $responsible_id = $data['responsible_id'];
-        $prefix = DB_PREFIX;
-        $sql = "UPDATE {$prefix}fe_customer SET
-            reg_type = '$reg_type',
-            status = '$status',
-            iin = '$iin',
-            company_name = '$company_name',
-            company_name_full = '$company_name_full',
-            official_address = '$official_address',
-            address = '$address',
-            client_category_id = '$client_category_id',
-            customer_type_id = '$customer_type_id',
-            responsible_id = '$responsible_id'
-            WHERE customer_id = '$customer_id'";
 
-        $result = $this->db->query($sql);
+        $this->db->query("UPDATE " . DB_PREFIX . "fe_customer SET reg_type = '" . (int)$data['reg_type'] . "', status = '" . (int)$data['status'] . "', iin = '" . $this->db->escape($data['iin']) . "', company_name = '" . $this->db->escape($data['company_name']) . "', company_name_full = '" . $this->db->escape($data['company_name_full']) . "', official_address = '" . $this->db->escape($data['official_address']) . "', address = '" . $this->db->escape($data['address']) . "', client_category_id = '" . (int)$data['client_category_id'] . "', customer_type_id = '" . (int)$data['customer_type_id'] . "', responsible_id = '" . (int)$data['responsible_id'] . "' WHERE customer_id = '" . (int)$customer_id . "'");
+
         return $customer_id;
     }
 
